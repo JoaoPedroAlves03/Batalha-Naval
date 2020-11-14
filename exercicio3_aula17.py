e5031dbec445208b0d0677c31d7a7b893e83ac0f
JoaoPedroAlves03 <74446394+JoaoPedroAlves03@users.noreply.github.com>
2020-11-14 04:11:05 -0300

    Create exercicio3_aula17.py

---
+++ exercicio3_aula17.py
@@ -0,0 +1,13 @@
+rep = int(input())
+i = 1
+
+while i < rep:
+    x = int(input())
+    y = int(input())
+    z = int(input())
+
+    for x in range(x, y, z)
+        print(x, end = '|')
+        
+    print()
+    i = i + 1
